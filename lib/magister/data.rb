50e5de1b9f7a87d1637d5c6c965aceed83f2bece
Bread <77288737+riley0122@users.noreply.github.com>
2024-02-18 22:14:11 +0100

    added get_classes function

--- lib/magister/data.rb
+++ lib/magister/data.rb
@@ -1,3 +1,22 @@
-class Data
-    
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+require 'date'
+
+def validate_date(date)
+    format = '%Y-%m-%d'
+    DateTime.strptime(date, format)
+    true
+rescue ArgumentError
+    false
+end
+
+module MagisterData
+    def get_classes(dateFrom, dateTo)
+        if validate_date(dateFrom) && validate_date(dateTo)
+            @profile.authenticatedRequest("/personen/{*id*}/afspraken?status=1&van=#{dateFrom}&tot=#{dateTo}")
+        else
+            puts "Invalid date, Format is yyyy-mm-dd"
+        end
+    end
 end
