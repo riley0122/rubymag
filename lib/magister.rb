6ac18043fc68fad3994ed233440b319b275f4d59
Bread <77288737+riley0122@users.noreply.github.com>
2024-02-18 22:14:18 +0100

    including data

--- lib/magister.rb
+++ lib/magister.rb
@@ -2,8 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 require './lib/magister/profile.rb'
+require './lib/magister/data.rb'
 
 class Magister
+    include MagisterData
+
     def initialize
         @profile = nil
     end
